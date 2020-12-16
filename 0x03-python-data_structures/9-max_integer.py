8da0208a28c9c196549501afdb18ce61a6a3bfe5
thormiwa <thormiwa04@gmail.com>
2020-12-16 09:29:21 +0000

    max interger

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max = my_list[0]
+    for a in range(len(my_list)):
+        if my_list[a] > max:
+            max = my_list[a]
+
+    return (max)
