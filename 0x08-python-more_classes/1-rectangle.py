fbf77506bd23036f833075b799737d33b5341821
thormiwa <thormiwa@gmail.com>
2021-01-11 20:57:42 +0000

    more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''define an empty class'''
+
+class Rectangle:
+    ''' a class named rectangle'''
+    
+    def __init__(self, width=0, height=0):
+        """
+        initialise the class
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        '''to retrieve height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """To set height."""
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @property
+    def width(self):
+        """To retrieve width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """To set width."""
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
